477eedb01d09a439ee37a650e51a83c6d2ba454d
Yutika Aggarwal <yaggarwal@unc.edu>
2021-09-06 13:42:59 -0400

    EX02

--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -1,6 +1,16 @@
 """Repeating a beat in a loop."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730480382"
 
 
 # Begin your solution here...
+
+beat: str = input("Insert beat: ")
+num_repeat: int = int(input("Number of times to repeat: "))
+if (num_repeat <= 0):
+    print("No beat...")
+else: 
+    while (num_repeat > 0):
+        print (beat)
+        num_repeat = num_repeat - 1
+
