c1c286ba8aaeebebb92fec2069acada1b8625df0
Yutika Aggarwal <yaggarwal@unc.edu>
2021-09-07 10:48:49 -0400

    progress

--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -5,6 +5,9 @@ __author__ = "730480382"
 
 # Begin your solution here...
 
+from _typeshed import HasFileno
+
+
 beat: str = input("Insert beat: ")
 num_repeat: int = int(input("Number of times to repeat: "))
 if (num_repeat <= 0):
@@ -13,4 +16,3 @@ else:
     while (num_repeat > 0):
         print (beat)
         num_repeat = num_repeat - 1
-
