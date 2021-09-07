e78ef0d753120d551c5be1b6fceb79b29651abfe
Yutika Aggarwal <yaggarwal@unc.edu>
2021-09-07 10:01:07 -0400

    CQ1 Done

---
+++ lessons/ChallengeQuestion1.py
@@ -0,0 +1,15 @@
+"""Challenge Question 1 Rewritten"""
+
+choice: int = int(input("Enter a number: "))
+
+if choice < 50:
+    if choice < 25: 
+        print("A")
+    else: 
+        print("B")
+
+else:
+    if choice > 75:
+        print("C")
+    else: 
+        print("D")
