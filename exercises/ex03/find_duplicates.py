ebf59ad5e0d68b8d6a05d1075d4606cc8f92a93b
Yutika Aggarwal <yaggarwal@unc.edu>
2021-09-14 01:41:14 -0400

    progress

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,20 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730480382"
+
+word: str = input("Enter a word: ")
+i: int = 0
+j: int = i+1
+duplicate_count: int = 0
+duplicate: bool = False
+while i < len(word):
+    while j < len(word):
+        if word[i] == word[j]:
+            duplicate_count = duplicate_count + 1
+        j = j+1
+    i = i+1
+if (duplicate_count > 0):
+    duplicate = True
+else:
+    duplicate = False
+print("Found duplicate: " + str(duplicate))
