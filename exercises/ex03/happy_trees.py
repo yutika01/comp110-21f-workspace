cca3242317b157d5ba20342f5335578a690a21d1
Yutika Aggarwal <yaggarwal@unc.edu>
2021-09-14 01:40:20 -0400

    progress ex03

--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,10 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730480382"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+i: int = 0
+depth: int = int(input("Depth: "))
+while (depth >= 0):
+    print(TREE)
--- exercises/ex03/tar_heels.py
+++ exercises/ex03/tar_heels.py
@@ -1,6 +1,14 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730480382"
 
-
-# Begin your solution here...
+tarheel_int: int = int(input("Enter an int: "))
+if (tarheel_int % 7 == 0):
+    if (tarheel_int % 2 == 0):
+        print("TAR HEELS")
+    else: 
+        print("HEELS")
+elif (tarheel_int % 2 == 0):
+    print("TAR")
+else:
+    print("CAROLINA")
