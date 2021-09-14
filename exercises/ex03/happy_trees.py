f88f8580d9576c0d7d708b446bbf69eb04b027a1
Yutika Aggarwal <yaggarwal@unc.edu>
2021-09-14 18:28:57 -0400

    progress on happy_trees

--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -4,7 +4,13 @@ __author__ = "730480382"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
-i: int = 0
 depth: int = int(input("Depth: "))
-while (depth >= 0):
-    print(TREE)
+line: int = 1
+num_print: int = 1
+output: str = ""
+while (line <= depth):
+    while (num_print <= line):
+        output = output + " " + TREE
+        num_print = num_print + 1
+    line = line + 1
+print (output)
