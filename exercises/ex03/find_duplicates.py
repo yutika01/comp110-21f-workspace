ea5032dfd113fc715671a3af5ffe74db1c345064
Yutika Aggarwal <yaggarwal@unc.edu>
2021-09-15 19:03:58 -0400

    EX03 done final

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -4,7 +4,7 @@ __author__ = "730480382"
 
 word: str = input("Enter a word: ")
 i: int = 0
-j: int = i+1
+j: int = i + 1
 duplicate_count: int = 0
 duplicate: bool = False
 while i < len(word):
@@ -12,8 +12,8 @@ while i < len(word):
     while j < len(word):
         if word[i] == word[j]:
             duplicate_count = duplicate_count + 1
-        j = j+1
-    i = i+1
+        j = j + 1
+    i = i + 1
 
 
 if (duplicate_count > 0):
