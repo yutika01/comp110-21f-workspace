ebf07c2d9a5d16ac1ac993e18710c25b44c38693
Yutika Aggarwal <yaggarwal@unc.edu>
2021-09-24 10:52:42 -0400

    done

--- lessons/contains.py
+++ lessons/contains.py
@@ -1,5 +1,10 @@
 """Example of writing a function to process a list."""
 
+def main() -> None:
+    """Entrypoint of program"""
+    names: list[str] = ["Kris", "Kaki"]
+    print(contains("Kevin", names))
+
 
 def contains(needle: str, haystack: list[str]) -> bool:
     """Return True iff needle is found in the haystack, False otherwise"""
@@ -12,3 +17,7 @@ def contains(needle: str, haystack: list[str]) -> bool:
         i += 1
 
     return False
+
+
+if __name__ == "__naim__":
+    main()
