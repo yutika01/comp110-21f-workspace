1d7a5e8a3ef4a22587dd3e25b3287827b8407bd1
Yutika Aggarwal <yaggarwal@unc.edu>
2021-09-29 12:49:14 -0400

    Changes1

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -11,15 +11,18 @@ def all(items: list[int], num: int) -> bool:
     if len(items) == 0:
         return False
     while i < len(items):
-        if list(i) != num:
+        if items[i] != num:
             return False
         else:
             i += 1
     return True
 
+
 def is_equal(list1: list[int], list2: list[int]) -> bool:
     """Determine whether the two lists are equal."""
     i: int = 0
+    if len(list1) and len(list2) == 0:
+        return True
     if len(list1) == len(list2):
         while i < len(list1) and i < len(list2): 
             if list1[i] == list2[i]:
@@ -27,6 +30,7 @@ def is_equal(list1: list[int], list2: list[int]) -> bool:
             else:
                 i += 1
     return False
+
 
 def max(inputlist: list[int]) -> int:
     """Determine maximum value in an input list."""
@@ -37,7 +41,7 @@ def max(inputlist: list[int]) -> int:
     maxvalue: int = inputlist[0]
     while i < len(inputlist):
         j = i + 1
-        while j < len(inputlist):
+        while j <= len(inputlist):
             if inputlist[i] < inputlist[j]:
                 maxvalue = inputlist[j]
             j += 1
