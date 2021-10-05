877b12f5950ce5862602aca12b59e2528b6c2e23
Yutika Aggarwal <yaggarwal@unc.edu>
2021-10-05 13:42:38 -0400

    Ready to submit

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,40 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730480382"
 
 # Define your functions below
+
+def only_evens(input: list[int]) -> list[int]:
+    """Return only evens in list."""
+    i: int = 0
+    for item in input:
+        if item % 2 != 0:
+            input.pop(i)
+        i+=1
+    return input
+
+
+def sub(input: list[int], start: int, end: int) -> list[int]:
+    """Return a subset of the original list."""
+    subset: list[int] = []
+    if start < 0:
+        start = 0
+    if end > len(input):
+        end = len(input)
+    if end <= 0:
+        return subset
+    i: int = start
+    while i < end:
+        subset.append(input[i])
+        i += 1
+    return subset
+
+
+def concat(list1: list[int], list2: list[int]) -> list[int]:
+    "Concatenate the two original lists into a new list."
+    joined: list[int] = []
+    for item in list1:
+        joined.append(item)
+    for item in list2:
+        joined.append(item)
+    return joined
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,42 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730480382"
+
+
+def test_only_evens() -> None: 
+    assert only_evens([10, 15, 20]) == [10, 20]
+
+
+def test_only_evens_again() -> None: 
+    assert only_evens([1, 2, 3]) == [2]
+
+
+def test_only_evens_again_two() -> None: 
+    assert only_evens([4, 4, 4]) == [4, 4, 4]
+
+
+def test_sub() -> None:
+    assert sub([3, 4, 5, 6], 1, 4) == [4, 5, 6]
+
+
+def test_sub_again() -> None:
+    assert sub([1, 2, 3, 4], -1, 3) == [1, 2, 3]
+
+
+def test_sub_again_two() -> None:
+    assert sub([10, 20, 30, 40], 1, 7) == [20, 30, 40]
+
+
+def test_concat() -> None:
+    assert concat([1, 2, 3, 4], [5, 6, 7, 8]) == [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+def test_concat_again() -> None:
+    assert concat([10, 20], [5, 4]) == [10, 20, 5, 4]
+
+
+def test_concat_again_two() -> None:
+    assert concat([], [3, 4, 5]) == [3, 4, 5]
