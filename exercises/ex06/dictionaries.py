cc47708ad0979a8fd49f6275d32e8482900832ac
Yutika Aggarwal <yaggarwal@unc.edu>
2021-10-19 17:57:45 -0400

    Check Invert

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,33 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730480382"
 
 # Define your functions below
+
+def invert(input: dict[str, str]) -> dict[str, str]:
+    final: dict[str, str]
+    final = {}
+    for key in input:
+        if input[key] in final:
+            raise KeyError("KeyError")
+        else:
+            final[input[key]] = key
+    return final
+
+def favorite_color(names: dict[str, str]) -> str:
+    popular: str = ""
+    for key in names:
+        for second in names:
+            if key == second:
+                print("yes")
+            else:
+                print("no")
+    return popular
+
+def count(input: list[str]) -> dict[str, int]:
+    final_count: dict[str, int]
+    final_count = {}
+    for item in input:
+        if item in final_count:
+            print("yes")
+    return final_count
